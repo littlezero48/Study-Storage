a5040630cbc01f57a58ac6d6c7271b5a5d1daeeb
littlezero48 <littlezero.048@gmail.com>
2023-03-17 20:13:35 +0900

    [FEAT] Controller 적용 #2

--- QueryDSL/src/main/java/com/example/querydsl/controller/ProductController.java
+++ QueryDSL/src/main/java/com/example/querydsl/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.example.querydsl.dto.SearchCondition;
 import com.example.querydsl.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
