02e7a8087bb2641ee94842a05df33c9789de66b9
littlezero48 <littlezero.048@gmail.com>
2023-04-09 19:57:43 +0900

    [Study] WebRTC 시그널링서버 구축 예제 추가

---
+++ WebRTC_SignalingServer/src/main/java/com/example/webrtc_signalingserver/WebRtcSignalingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webrtc_signalingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcSignalingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRtcSignalingServerApplication.class, args);
+    }
+
+}
